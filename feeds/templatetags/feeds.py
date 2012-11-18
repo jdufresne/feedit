ecb669a9686d229c1066038e59910a137ca1fdb0
Jon Dufresne <jon@jondufresne.org>
2012-11-18 12:41:44 -0800

    Now shows number of unread items

--- feeds/templatetags/feeds.py
+++ feeds/templatetags/feeds.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.inclusion_tag('feeds/tags/sidebar.html')
 def sidebar(user):
     return {'user': user}
+
+
+@register.inclusion_tag('feeds/tags/feed_link.html')
+def feed_link(user, feed):
+    return {'feed': feed, 'unread': feed.unread_by(user).count()}
