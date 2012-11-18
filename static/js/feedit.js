2b33f724f74210eacde50a800609a43c4bdadb70
Jon Dufresne <jon@jondufresne.org>
2012-11-17 20:10:41 -0800

    Now reads entries as they scroll into view

--- static/js/feedit.js
+++ static/js/feedit.js
@@ -1,17 +1,41 @@
 (function($) {
 	$(document).ready(function() {
 		$('.entry').find('form').hide();
-	});
 
-	$(document).on('click', '.entry', function() {
-		$('.active').removeClass('active');
-		var self = $(this);
-		self.addClass('active');
-		var form = self.find('form');
-		$.ajax(form.attr('action'), {
-			'type': 'POST',
-			'dataType': 'json',
-			'data': form.serializeArray()
+		$(document).on('click', '.entry', function() {
+			readEntry(this);
+		});
+
+		var SCROLL_THRESHOLD = 30;
+		$(window).scroll(function() {
+			scroll = $(this).scrollTop()
+			$('.entry').each(function() {
+				var diff = $(this).offset().top - scroll;
+				if (Math.abs(diff) < SCROLL_THRESHOLD)
+				{
+					readEntry(this);
+				}
+			});
 		});
 	});
+
+	function readEntry(el) {
+		el = $(el);
+		$('.active').removeClass('active');
+		el.addClass('active');
+		if (!el.is('.read'))
+		{
+			el.addClass('read');
+			submitAjax(el.find('form'));
+		}
+	}
+
+	function submitAjax(el) {
+		el = $(el).closest('form');
+		$.ajax(el.attr('action'), {
+			'type': el.attr('method'),
+			'dataType': 'json',
+			'data': el.serializeArray()
+		});
+	}
 })(jQuery);
